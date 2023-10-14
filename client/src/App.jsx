a75ebbe9ec6e226c7666d4269b8ecc173651c1ad
Yonxxe Lenovo <dominijisumbonafe@gmail.com>
2023-10-14 19:13:07 +0800

    just added a client section

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+
+import{BrowserRouter as Router, Route, Routes} from "react-router-dom"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Router>
+        Header
+        <Route>
+          <Routes>
+
+          </Routes>
+        </Route>
+      </Router>
+    </>
+  )
+}
+
+export default App
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const cors = require('cors')
 const express = require('express')
 const connectDB = require('./connectDB')
+const Book = require('./models/Books')
 const app = express()
 const PORT = process.env.PORT || 8000
 
@@ -11,8 +12,19 @@ app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
+
 app.get('/', (req, res) => {
     res.json("hey app")
+})
+app.get('/api/books', async (req, res) => {
+    try{
+        const data = await Book.find({}).limit(2);
+        res.json(data)
+
+    }
+    catch(error){
+       res.status(500).json({error})
+    }
 })
 app.get('*', (req, res) => {
     res.sendStatus("404")
